959ec89b1ffcf76eeb573a48379b72b9fe608250
¨lord¨ <yrnlehjend@gmail.com>
2023-06-01 21:59:17 +0000

    add function to add new at the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,26 +9,34 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-	binary_tree_t *temp;
-	
-	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	if (new_node == NULL || parent == NULL)
-	{
-		return (NULL);
-	}
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->n = value;
-	if (parent->left == NULL)
-	{
-		parent->left = new_node;
-		return (new_node);
-	}
-	temp = parent->left;
-	parent->left = new_node;
-	new_node->left = temp;
-	return (new_node);
+	binary_tree_t *new_node, *temp;
 
+	if (parent == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		temp = parent->left;
+		new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->parent = parent;
+		new_node->left = temp;
+		new_node->right = NULL;
+		new_node->n = value;
+		temp->parent = new_node;
+		parent->left = new_node;
+	}
+	else
+	{
+		new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->n = value;
+		parent->left = new_node;
+	}
+	return (new_node);
 }
+
