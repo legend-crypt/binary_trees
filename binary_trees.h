1e199c088cd347e1173d4d81dbb9b36421b2f90c
¨lord¨ <yrnlehjend@gmail.com>
2023-06-02 12:46:12 +0000

    refactor code

--- binary_trees.h
+++ binary_trees.h
@@ -28,6 +28,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
-void binary_tree_print(const binary_tree_t *tree);
-//static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 #endif /* BINARY_H */
