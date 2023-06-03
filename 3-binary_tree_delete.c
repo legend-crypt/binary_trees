19d387a84a9be51757557f057caf21da1e5447a2
¨lord¨ <yrnlehjend@gmail.com>
2023-06-03 19:20:30 +0000

    add function to delete nodes

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_delete - This function deletes a binary tree
+ * @tree - the tree to be deleted
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}	
+}
+
