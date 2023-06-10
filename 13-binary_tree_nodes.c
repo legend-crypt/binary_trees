023f7a5c143e62e40c519db8b06ee09bfef97568
¨lord¨ <yrnlehjend@gmail.com>
2023-06-10 09:48:25 +0000

    add function to count tree with atleast one children

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function that returns the number of nodes with children
+ * @tree: tree to check
+ * Return: number of nodes with children
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t nodes = 0;
+
+	if (tree->left != NULL || tree->right != NULL)
+		nodes += 1;
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
